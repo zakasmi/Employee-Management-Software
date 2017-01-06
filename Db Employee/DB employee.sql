c2991bd478bb89b6ebfcb5a91cab887465d8833c
dell <dell@DELL>
2017-01-06 06:18:08 +0000

    Pays region Ville combobox  selectedvaluechanged

--- Db Employee/DB employee.sql
+++ Db Employee/DB employee.sql
@@ -1,41 +1,94 @@
 use master 
-create database GestEmp01
+create database GestEmp
 
-use GestEmp01
+
+
+use GestEmp
+--============= Creation des tables =============--
+
 create table Employee(
-No_emp varchar(4) constraint PK_Employee Primary key,
+ID_EMP varchar(10) constraint PK_Employee Primary key,
 Nom varchar(20),
 Prenom varchar(20),
 DateN date,
-Gendre char(2),
+Sexe char(2),
 Adress varchar(50),
 Date_Emb date,
 Salaire Money,
-Photo_emp binary,
-Dept_Num varchar(4) constraint FK_Employee_Dpt Foreign key references Departement (Dept_Num) on delete cascade on update cascade,
-Post_num varchar(4) constraint FK_Employee_poste Foreign key references poste (Post_num)on delete cascade on update cascade,
-Id_pays int constraint FK_Employee_pays Foreign key references Pays(Id_pays)
+Photo binary,
+id_dept varchar(10) constraint FK_Employee_Dpt Foreign key references Departement (ID_DEPT) on delete cascade on update cascade,
+id_post varchar(10) constraint FK_Employee_poste Foreign key references poste (ID_POST)on delete cascade on update cascade,
+Id_pays int constraint FK_Employee_pays Foreign key references Pays(ID_PAYS)
 )
 
-use GestEmp01
 create table pays(
 
-Id_pays int identity(1,1) constraint PK_Pays Primary key,
-Nom_pay nvarchar(50)
+ID_PAYS int identity(1,1) constraint PK_Pays Primary key,
+Nom_pays nvarchar(50)
 
 )
-insert into pays values 
-('Morroco')
-
-
 
 create table region (
 
-id_region int identity (1,1) constraint PK_region Primary key,
-nom_region nvarchar(50),
-Id_pays int constraint FK_region foreign key references pays (Id_pays)
+ID_Region int identity (1,1) constraint PK_region Primary key,
+Nom_region nvarchar(50),
+id_pays int constraint FK_region foreign key references pays (Id_pays)
 
 )
+
+create table ville (
+
+ID_VILLE int identity (1,1)constraint Pk_ville Primary key,
+Nom_ville varchar(50),
+id_region int constraint FK_ville Foreign key references region(id_region) on delete cascade on update cascade
+)
+
+create table Departement (
+
+ID_DEPT varchar(10) constraint Pk_Departement Primary key,
+Dept_Nom varchar(20),
+Lieu varchar(10)
+
+)
+
+create table poste (
+
+ID_POST varchar(10) constraint Pk_poste primary key,
+Post_nom varchar(30)
+)
+
+create table Compte(
+
+ID_COMPT int identity constraint PK_compte Primary key,
+Username varchar(30),
+[password] varchar(30)
+
+)
+
+Alter table Compte
+Add constraint Ck_Username Check(Username not like '%['']%' and Username not like '%["]%'),
+constraint Ck_Password Check ([Password] not like '%['']%' and [Password]not like '%["]%')
+
+
+--============= Secutier =============--
+
+create login L_Elmansari
+with password = '123'
+
+create login L_Kasmi
+with password = '456'
+
+Create user u_zakaria for login L_Kasmi
+Create user u_Usef for login L_Elmansari
+
+sp_addrolemember 'db_owner','u_zakaria';
+sp_addrolemember 'db_owner','u_Usef';
+
+
+--============= Insertion =============-- 
+
+insert into pays values 
+('Morroco')
 
 insert into region values 
 ('Oued Ed-Dahab - Lagouira',1),
@@ -428,68 +481,11 @@ insert into ville values
 ('Sidi Lyamani ',16),
 ('Zinat ',16)
 
-create table ville (
-
-Id_ville int identity (1,1)constraint Pk_ville Primary key,
-Nom_ville varchar(50),
-id_region int constraint FK_ville Foreign key references region(id_region) on delete cascade on update cascade
-)
-
-
-create table Departement (
-
-Dept_Num varchar(4) constraint Pk_Departement Primary key,
-Dept_Nom varchar(20),
-Lieu varchar(10)
-
-)
-
-create table poste (
-
-Post_num varchar(4) constraint Pk_poste primary key,
-Post_nom varchar(30)
-)
-
-
-create table Compte(
-
-idCompt int identity constraint PK_compte Primary key,
-username varchar(30),
-[password] varchar(30)
-
-)
-
-
-
-
-
-Alter table Compte
-Add constraint Ck_Username Check(Username not like '%['']%' and Username not like '%["]%'),
-constraint Ck_Password Check ([Password] not like '%['']%' and [Password]not like '%["]%')
-
-
-
-
-
-
-create login L_Elmansari
-with password = '123'
-
-create login L_Kasmi
-with password = '456'
-
-Create user u_zakaria for login L_Kasmi
-Create user u_Usef for login L_Elmansari
-
-sp_addrolemember 'db_owner','u_zakaria';
-sp_addrolemember 'db_owner','u_Usef';
-
-
-
-
-
-select * from region
-
+insert into Departement values
+('DEP1','science','Oujda'),
+('DEP2','Physique','Berkan'),
+('DEP3','sport','Nador'),
+('DEP4','mechanique','Taourirt')
 
 
 
